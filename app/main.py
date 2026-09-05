af279952e03530caf4c2448f5e0e0e89b8b32ad7
Prakash Chaudhary <01prakashchaudharyc@gmail.com>
2026-09-05 20:34:17 +0545

    feat: add get task by id endpoint

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, status
+from fastapi import FastAPI, status, HTTPException
 from app.models import Task, TaskCreate
 
 app = FastAPI(title="SWE project")
@@ -25,6 +25,17 @@ def root() -> dict:
 @app.get("/tasks", response_model=list[Task])
 def get_tasks():
     return tasks
+
+@app.get("/tasks/{task_id}", response_model=Task)
+def get_tasks(task_id: int):
+    for task in tasks:
+        if task["id"] == task_id:
+            return task
+
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="task not found!",
+    )
 
 @app.post("/tasks", response_model=Task, status_code=status.HTTP_201_CREATED,)
 def create_task(task: TaskCreate):
