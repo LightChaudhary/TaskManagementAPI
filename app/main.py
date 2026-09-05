cc798e161cced6874098759baad7cbed094ace69
Prakash Chaudhary <01prakashchaudharyc@gmail.com>
2026-09-05 17:24:56 +0545

    feat: add basic task API endpoints

--- app/main.py
+++ app/main.py
@@ -1,7 +1,39 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+from app.models import Task, TaskCreate
 
 app = FastAPI(title="SWE project")
+
+tasks = [
+    {
+        "id": 1,
+        "title": "learn rest",
+        "description" : "understand http methods and status codes.",
+        "completed": False,
+    },
+    {
+        "id" : 2,
+        "title": "learn git branches",
+        "description": "practice feature branches and merging",
+        "completed": False,
+    },
+]
 
 @app.get("/")
 def root() -> dict:
     return {"message" : "Hello!, Prakash"}
+
+@app.get("/tasks", response_model=list[Task])
+def get_tasks():
+    return tasks
+
+@app.post("/tasks", response_model=Task, status_code=status.HTTP_201_CREATED,)
+def create_task(task: TaskCreate):
+    new_task = {
+        "id" : len(tasks) + 1,
+        "title" : task.title,
+        "description" : task.description,
+        "completed" : task.completed,
+    }
+    tasks.append(new_task)
+
+    return new_task
---
+++ app/models.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+class TaskCreate(BaseModel):
+    title: str
+    description: str
+    completed: bool = False
+
+
+class Task(BaseModel):
+    id: int
+    title: str
+    description: str
+    completed: bool
