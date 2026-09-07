dfc6192faee9f7e159f5e9cfe57ba82932e661af
Prakash Chaudhary <01prakashchaudharyc@gmail.com>
2026-09-07 20:29:20 +0545

    feat: add database configuration

---
+++ app/database.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+DATABASE_URL = "sqlite:///./tasks.db"
+
+engine = create_engine(
+    DATABASE_URL, 
+    connect_args={"check_same_thread": False},
+)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False,
+)
+
+
+class Base(DeclarativeBase):
+    pass
+
