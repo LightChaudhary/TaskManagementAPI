1324344b314f84a6a4a965f7d13a3a41bfadd7f3
Prakash Chaudhary <01prakashchaudharyc@gmail.com>
2026-09-07 20:31:14 +0545

    feat: use database for task CRUD

--- app/api/tasks.py
+++ app/api/tasks.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, status, HTTPException
 
-from app.models import task as task_model
+from app.models.task import Task
+from app.database import SessionLocal
 from app.schemas.task import TaskCreate, TaskOut, TaskUpdate
 
 router = APIRouter(prefix="/tasks", tags=["Tasks"])
@@ -11,64 +12,90 @@ router = APIRouter(prefix="/tasks", tags=["Tasks"])
     status_code=status.HTTP_201_CREATED,
 )
 def create_task(task: TaskCreate) -> TaskOut:
-    new_task = {
-        "id": task_model.next_task_id,
-        "title": task.title,
-        "description": task.description,
-        "status": task.status,
-        "priority": task.priority,
-    }
+    db = SessionLocal()
 
-    task_model.tasks[task_model.next_task_id] = new_task
-    task_model.next_task_id += 1
+    new_task = Task(
+        title=task.title,
+        description=task.description,
+        status=task.status.value,
+        priority=task.priority.value,
+    )
+
+    db.add(new_task)
+    db.commit()
+    db.refresh(new_task)
+    db.close()
 
     return new_task
 
 @router.get("", response_model=list[TaskOut])
 def get_tasks() -> list[TaskOut]:
-    return list(task_model.tasks.values())
+    db = SessionLocal()
+
+    tasks = db.query(Task).all()
+
+    db.close()
+
+    return tasks
 
 @router.get("/{task_id}", response_model=TaskOut)
 def get_task(task_id: int) -> TaskOut:
-    task = task_model.tasks.get(task_id)
+    db = SessionLocal()
+
+    task = db.get(Task, task_id)
 
     if task is None:
+        db.close()
+
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="task not found!",
         )
+
+    db.close()
 
     return task
 
 @router.put("/{task_id}", response_model=TaskOut)
 def update_task(task_id: int, task: TaskUpdate) -> TaskOut:
-    existing_task = task_model.tasks.get(task_id)
+    db = SessionLocal()
+
+    existing_task = db.get(Task, task_id)
 
     if existing_task is None:
+        db.close()
+
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="task not found!",
         )
 
-    existing_task.update(
-        {
-            "title": task.title,
-            "description": task.description,
-            "status": task.status,
-            "priority": task.priority,
-        }
-    )
+    existing_task.title = task.title
+    existing_task.description = task.description
+    existing_task.status = task.status.value
+    existing_task.priority = task.priority.value
+
+    db.commit()
+    db.refresh(existing_task)
+    db.close()
+
 
     return existing_task
 
 @router.delete("/{task_id}", status_code=status.HTTP_204_NO_CONTENT,)
 def delete_task(task_id: int) -> None:
-    task = task_model.tasks.get(task_id)
+    db = SessionLocal()
+
+    task = db.get(Task, task_id)
 
     if task is None:
+        db.close()
+
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="task not found!",
         )
 
-    del task_model.tasks[task_id]
+    db.delete(task)
+    db.commit()
+    db.close()
