4c42dd1c11006c7f08acdfafcdd4cba439e0eb2b
Prakash Chaudhary <01prakashchaudharyc@gmail.com>
2026-09-07 20:31:54 +0545

    feat: intialize database tables

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
-
 from app.api.tasks import router as task_router
+
+from app.database import Base, engine
+from app.models.task import Task
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="SWE Project")
 
